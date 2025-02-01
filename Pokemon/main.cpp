b2454d42d411369fe94ee5d2b3eb1e6d16d46249
Pankaj <Pankajsawant2258@gmail.com>
2025-02-01 18:30:45 +0530

    changes

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+using namespace std;
+
+string player_name;
 
 int main() {
-
+    cout << "Enter your name\n";
+    cin >> player_name;
+    cout << player_name;
     return 0;
 }
