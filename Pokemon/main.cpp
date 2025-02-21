f6a30424524db6faf1378b88b1e8483cbc515a8f
Pankaj <Pankajsawant2258@gmail.com>
2025-02-21 21:05:08 +0530

    added enum

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
+enum PokemonChoice {
+    Charmander, Bulbasaur, Squirtle, Invalid_Choice
+    };
     string player_name;
     int playerChoice;
     int confirmation;
-
+    
     int main() {
+        PokemonChoice player_choice = Invalid_Choice;
         cout << "OAK : Hello my name is Oak. You can call me Mr. Oak or Professor Oak. I am here to introduce you to the world of pokemon.\nAre you ready?\nFirst tell me what is your name.\n";
         cin >> player_name;
         cout << "OAK : Hello " + player_name + ".\nGlad to meet you. Here are 3 pokemons that are left. Sorry these are the only ones remaining. They are not weak.";
@@ -18,15 +22,19 @@ using namespace std;
         {
         case 1:
             cout << "OAK : So you want Charmender. Are you sure? \n1. Yes\n2. No";
+            player_choice = Charmander;
             break;
         case 2:
             cout << "OAK : So you want Bulbasaur. Are you sure? \n1. Yes\n2. No";
+            player_choice = Bulbasaur;
             break;
         case 3:
             cout << "OAK : So you want Squirtle. Are you sure? \n1. Yes\n2. No";
+            player_choice = Squirtle;
             break;
         default:
-            cout << "Invalid choice Bro BYE";
+            cout << "Take charmander as punishment";
+            player_choice = Charmander;
             break;
         }
 
