f6145316f0f48d2179d5b3d370aba75896fcc9ad
Pankaj <Pankajsawant2258@gmail.com>
2025-03-29 14:47:22 +0530

    removed commented classes

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -5,88 +5,9 @@
 #include "PokemonChoice.hpp"
 #include "Utility.hpp"
 #include "Pokemon.hpp"
-//#include "Player.hpp"
+
+//I made changes some other way
 using namespace std;
-
-//class Pokemon 
-//{
-//public:
-//    string name;
-//    PokemonType type;
-//    int health;
-//
-//    Pokemon() 
-//    {
-//        name = "Pikachu";
-//        type = PokemonType::ElectricType;
-//        health = 10;
-//    }
-//    Pokemon(string p_Name, PokemonType p_Type, int p_Health) 
-//    {
-//        name = p_Name;
-//        type = p_Type;
-//        health = p_Health;
-//    }
-//    // Copy constructor
-//    Pokemon(const Pokemon& other) {
-//        name = other.name;
-//        type = other.type;
-//        health = other.health;
-//        cout << "A new Pokemon has been copied from " << other.name << "!\n";
-//    }
-//
-//    // Destructor
-//    ~Pokemon() {
-//        //cout << name << " has been released.\n";
-//    }
-//    void attack() 
-//    {
-//        cout << name << " Attacks With Powerful Attack!!\n"; //<< endl;
-//    }
-//};
-
-//class Player
-//{
-//public:
-//    string p_Name;
-//    Pokemon p_ChosenPokemon;
-//
-//    Player() 
-//    {
-//        p_Name = "Trainer";
-//        p_ChosenPokemon = Pokemon();
-//    }
-//
-//    Player(string newName, Pokemon newChosenPokemon)
-//    {
-//        p_Name = newName;
-//        p_ChosenPokemon = newChosenPokemon;
-//    }
-//
-//    Player(Player& otherPlayer)
-//    {
-//        p_Name = otherPlayer.p_Name;
-//        p_ChosenPokemon = otherPlayer.p_ChosenPokemon;
-//    }
-//
-//    void choosePokemon(int choice) {
-//        switch (choice) {
-//        case 1:
-//            p_ChosenPokemon = Pokemon("Charmander", PokemonType::FireType, 100);
-//            break;
-//        case 2:
-//            p_ChosenPokemon = Pokemon("Bulbasaur", PokemonType::GrassType, 100);
-//            break;
-//        case 3:
-//            p_ChosenPokemon = Pokemon("Squirtle", PokemonType::WaterType, 100);
-//            break;
-//        default:
-//            p_ChosenPokemon = Pokemon("Pikachu", PokemonType::ElectricType, 100);
-//            break;
-//        }
-//        //cout << "Player " << p_Name << " chose " << p_ChosenPokemon.name << "!\n";
-//    }
-//};
 
 class ProfessorOak
 {
